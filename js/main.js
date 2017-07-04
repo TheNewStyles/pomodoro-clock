3318418f7245bf4e0aa1a1463cf011be9a39b2c5
TheNewStyles <tyler.peterson@utah.edu>
2017-07-04 10:43:48 -0600

    Add start time div and time

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-document.getElementById("start").addEventListener('click', function() {
+var timer = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var startTime = new Date().setMinutes(25);
+
+timer.innerHTML = Math.floor((startTime % (1000 * 60 * 60)) / (1000 * 60)) + "m " + Math.floor((startTime % (1000 * 60)) / 1000) + " s";
+
+
+startButton.addEventListener('click', function() {
     // Set the date we're counting down to
     var countDownDate = new Date().setMinutes(25);
 
@@ -19,12 +26,12 @@ document.getElementById("start").addEventListener('click', function() {
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id="demo"
-        document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+        timer.innerHTML = minutes + "m " + seconds + "s ";
 
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
+            timer.innerHTML = "EXPIRED";
         }
     }, 1000);
 }, false);
