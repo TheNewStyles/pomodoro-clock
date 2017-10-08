0b875e9c12842ed28cc2c3a7b9eb9f93a6acd1a5
TheNewStyles <tyler.peterson@utah.edu>
2017-10-08 16:27:00 -0600

    Update coding style problems

--- js/main.js
+++ js/main.js
@@ -17,56 +17,57 @@ const breakMinsUpButton = document.getElementById("break_work_up");
 const breakMinsDownButton = document.getElementById("break_work_down");
 
 const zeroStr = "0";
-let counter=0;
+let counter = 0;
 let hasReset = false;
 let isRunning = false;
 
 //event listeners
-workMinsUpButton.addEventListener("click", function () {
-    if(isRunning) return;
+workMinsUpButton.addEventListener("click", () => {
+    if (isRunning) return;
     workMinutesCount = increaseTime(workMinutesCount, workMinsDiv);
 });
 
-workMinsDownButton.addEventListener("click", function () {
-    if(isRunning) return;
+workMinsDownButton.addEventListener("click", () => {
+    if (isRunning) return;
     workMinutesCount = decreaseTime(workMinutesCount, workMinsDiv);
 });
 
-breakMinsUpButton.addEventListener("click", function () {
-    if(isRunning) return;
+breakMinsUpButton.addEventListener("click", () => {
+    if (isRunning) return;
     breakMinutesCount = increaseTime(breakMinutesCount, breakMinsDiv);
 });
 
-breakMinsDownButton.addEventListener("click", function () {
-    if(isRunning) return;
+breakMinsDownButton.addEventListener("click", () => {
+    if (isRunning) return;
     breakMinutesCount = decreaseTime(breakMinutesCount, breakMinsDiv);
 });
 
-startButton.addEventListener("click", function() {
-    if(isRunning) return;
+startButton.addEventListener("click", () => {
+    if (isRunning) return;
     startTimer(workMinutesCount, workMinsDiv, workSecsDiv);
 }, false);
 
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", () => {
     resetTime(25, 5, "00");
 }, false);
 
 //helper functions
-function getTime(minutes) {
+function getTime (minutes) {
     const time = new Date();
     return time.setMinutes(time.getMinutes() + minutes);
 }
 
-function startTimer(minutes, minutesDiv, secondsDiv) {
+function startTimer (minutes, minutesDiv, secondsDiv) {
+
     const countDownDate = getTime(minutes);
     counter++;
     hasReset = false;
     isRunning = true;
 
-    const j = setInterval(function() {
+    const interval = setInterval(() => {
 
         if (hasReset) {
-            clearInterval(j);
+            clearInterval(interval);
             return;
         }
 
@@ -76,70 +77,70 @@ function startTimer(minutes, minutesDiv, secondsDiv) {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        if(minutes <= 9){
+        if (minutes <= 9) {
             const minutesStr = `${minutes}`;
-            minutesDiv.innerHTML = zeroStr+minutesStr;
-        }
-        else{
+            minutesDiv.innerHTML = zeroStr + minutesStr;
+        } else {
             minutesDiv.innerHTML = minutes;
         }
 
-        if(seconds <= 9){
+        if (seconds <= 9) {
             const secondsStr = `${seconds}`;
-            secondsDiv.innerHTML = zeroStr+secondsStr;
-        }else{
+            secondsDiv.innerHTML = zeroStr + secondsStr;
+        } else {
             secondsDiv.innerHTML = seconds;
         }
 
         if (distance < 0) {
-            clearInterval(j);
+            clearInterval(interval);
             minutesDiv.innerHTML = "00";
             secondsDiv.innerHTML = "00";
 
-            if(counter<2){
+            if (counter < 2) {
                 startTimer(breakMinutesCount, breakMinsDiv, breakSecsDiv);
             }
         }
     }, 100);
+
 }
 
-function decreaseTime(count, div) {
-    if(count > 1){
-        count--;
+function decreaseTime (count, div) {
+    if (count > 1) {
+        count--;    
     }
-    if(count <= 9){
+    if (count <= 9) {
         const countStr = `${count}`;
-        div.innerText = zeroStr+countStr;
-    }else{
-        div.innerText = count;
+        div.innerText = zeroStr + countStr;    
+    } else {
+        div.innerText = count;    
     }
-
     return count;
 }
 
-function increaseTime(count, div) {
-    if(count <= 60){
-        count++;
+function increaseTime (count, div) {
+    if (count <= 60) {
+        count++;    
     }
-    if(count <= 9){
+    if (count <= 9) {
         const countStr = `${count}`;
-        div.innerText = zeroStr+countStr;
-    }else{
-        div.innerText = count;
+        div.innerText = zeroStr + countStr;    
+    } else {
+        div.innerText = count;    
     }
-
     return count;
 }
 
-function resetTime(workMinutes, breakMinutes, seconds) {
+function resetTime (workMinutes, breakMinutes, seconds) {
+
     hasReset = true;
     isRunning = false;
     const breakMinutesStr = `${breakMinutes}`;
 
     workMinsDiv.innerHTML = workMinutes;
     workSecsDiv.innerHTML = seconds;
-    breakMinsDiv.innerHTML = zeroStr+breakMinutesStr;
+    breakMinsDiv.innerHTML = zeroStr + breakMinutesStr;
     breakSecsDiv.innerHTML = seconds;
     workMinutesCount = workMinutes;
     breakMinutesCount = breakMinutes;
+
 }
